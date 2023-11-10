4251522850f406a06e624a5392c90ab63bc4ef02
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-11-10 12:36:20 -0500

    shortened TerracedFields

--- Kattis/TerracedFields.cpp
+++ Kattis/TerracedFields.cpp
@@ -1,13 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-int countDig(long long n) {
-    int digs = 0;
-    while (n) digs++, n /= 10;
-    return digs;
-}
-
-int count6and8(long long n) {
+int count6and8(ll n) {
     int ans = 0;
     while (n) {
         if (n % 10 == 6 || n % 10 == 8) ans++;
@@ -19,18 +14,13 @@ int count6and8(long long n) {
 int main() {
     int t; cin >> t;
     while (t--) {
-        long long n; cin >> n;
+        ll n; cin >> n;
 
-        int digs = max(3, countDig(n)); vector<long long> num;
-
-        long long tmp = n;
-        for (int i = 0; i < max(3,digs); ++i) num.push_back(tmp % 10), tmp /= 10;
+        vector<ll> num; ll tmp = n; int digs = 0;
+        while (digs < 3 || tmp) num.push_back(tmp % 10), tmp /= 10, digs++;
         reverse(num.begin(), num.end());
 
-        long long dp[19][2];
-        for (int i = 0; i < 19; ++i) memset(dp[i], 0, sizeof(long long) * 2);
-        long long dp1[19][2];
-        for (int i = 0; i < 19; ++i) memset(dp1[i], 0, sizeof(long long) * 2);
+        vector<vector<ll>> dp(19, vector<ll>(2)), dp1(19, vector<ll>(2));
 
         dp[0][1] = 1;
         for (int i = 0; i < digs - 3; ++i) {
@@ -50,7 +40,7 @@ int main() {
         }
         
         int bound = num[digs - 3] * 100 + num[digs - 2] * 10 + num[digs - 1];
-        long long ans = 0;
+        ll ans = 0;
         for (int i = 0; i <= 999; ++i) {
             if (i > bound) {
                 if (i % 8 == 0) 

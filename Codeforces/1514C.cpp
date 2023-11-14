ec47a7cddd4dcbc42c47c2630e3d0c991b82a85e
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-11-14 13:05:33 -0500

    1514C done

---
+++ Codeforces/1514C.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n; set<int> a;
+    long long prod = 1;
+    for (int i = 1; i < n - 1; ++i) {
+        if (gcd(i, n) == 1) {
+            prod = (prod * i) % n;
+            a.insert(i);
+        }
+    }
+
+    // last co-prime : (n - 1) is always co-prime to n
+    if ((prod * (n - 1)) % n == 1) a.insert(n - 1);
+
+    cout << a.size() << endl;
+    for (auto& v : a) cout << v << " ";
+    return 0;
+}

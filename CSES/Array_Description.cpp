875814ee29c8ab6c5d7779621d43704e856c9d0d
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-11-15 04:06:06 -0500

    shortened Array_Description

--- CSES/Array_Description.cpp
+++ CSES/Array_Description.cpp
@@ -7,32 +7,23 @@ int main() {
     int n, m; cin >> n >> m; for (int i = 0; i < n; ++i) cin >> a[i];
 
     // dp(i, j): number of arrays for which ith element = j
-    vector<vector<int>> dp(n, vector<int>(m + 1));
+    int dp[n][m + 1]; fill(dp[0], dp[0] + m + 1, 0);
 
-    if (a[0]) 
-        dp[0][a[0]] = 1;
-    else
-        for (int i = 1; i <= m; ++i) dp[0][i] = 1;
+    for (int i = 1; i <= m; ++i)
+        if (i == a[0] || !a[0]) dp[0][i] = 1;
     
     for (int i = 1; i < n; ++i) {
-        if (a[i]) {
-            dp[i][a[i]] = dp[i - 1][a[i]];
-            if (a[i] - 1) dp[i][a[i]] = (dp[i][a[i]] + dp[i - 1][a[i] - 1]) % MOD;
-            if (a[i] + 1 <= m) dp[i][a[i]] = (dp[i][a[i]] + dp[i - 1][a[i] + 1]) % MOD;
-        }
-        else {
-            for (int j = 1; j <= m; ++j) {
-                dp[i][j] = dp[i - 1][j];
-                if (j - 1) dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % MOD;
-                if (j + 1 <= m) dp[i][j] = (dp[i][j] + dp[i - 1][j + 1]) % MOD;
-            }
+        fill(dp[i], dp[i] + m + 1, 0);
+        for (int j = 1; j <= m; ++j) {
+            if (a[i] && j != a[i]) continue;
+            dp[i][j] = dp[i - 1][j];
+            if (j - 1) dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % MOD;
+            if (j + 1 <= m) dp[i][j] = (dp[i][j] + dp[i - 1][j + 1]) % MOD;
         }
     }
 
-    int ans = 0;
-    for (int i = 0; i <= m; ++i) ans = (ans + dp[n - 1][i]) % MOD;
-
-    cout << ans;
+    for (int i = 1; i <= m; ++i) dp[n - 1][i] = (dp[n - 1][i] + dp[n - 1][i - 1]) % MOD;
+    cout << dp[n - 1][m];
 
     return 0;
 }

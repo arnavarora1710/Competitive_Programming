c357c27eb3f425fd4b0231411c5e301eecf57238
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-11-17 13:17:05 -0500

    idk

--- CSES/Counting_Towers.cpp
+++ CSES/Counting_Towers.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MOD = 1e9 + 7;
-const int MAXN = 1e6 + 1;
+const int MOD = 1e9 + 7, MAXN = 1e6 + 1;
 long dp[MAXN], dp1[MAXN];
 
 int main() {

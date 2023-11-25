deee0185443b115feb686df2e00128d083a46f0c
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-11-25 01:48:36 -0500

    done 1901A

---
+++ Codeforces/1901A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 50;
+int t, n, x, a[MAXN];
+
+int main() {
+    cin >> t;
+    while (t--) {
+        cin >> n >> x; for (int i = 0; i < n; ++i) cin >> a[i];
+        int ans = max(a[0], (x - a[n - 1]) * 2);
+        for (int i = 0; i < n - 1; ++i) ans = max(ans, a[i + 1] - a[i]);
+        cout << ans << endl;
+    }
+    return 0;
+}

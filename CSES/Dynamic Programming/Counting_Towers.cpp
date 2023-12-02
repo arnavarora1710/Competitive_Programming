55a31988609726744ddbf9477ddc79e84bdfba39
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-12-01 20:12:34 -0500

    removed unnecessary code from Counting_Towers

--- CSES/Dynamic Programming/Counting_Towers.cpp
+++ CSES/Dynamic Programming/Counting_Towers.cpp
@@ -4,13 +4,11 @@ const int MOD = 1e9 + 7, MAXN = 1e6 + 1;
 long dp[MAXN], dp1[MAXN];
 
 int main() {
-    fill(dp, dp + MAXN, 0); fill(dp1, dp1 + MAXN, 0);
     dp[1] = 1, dp1[1] = 1;
     for (int i = 2; i < MAXN; ++i) {
         dp[i] = ((2 * dp[i - 1]) % MOD + dp1[i - 1]) % MOD;
         dp1[i] = ((4 * dp1[i - 1]) % MOD + dp[i - 1]) % MOD;
     }
-
     int t; cin >> t;
     while (t--) {
         int n; cin >> n;

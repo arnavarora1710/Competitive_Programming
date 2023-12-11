93fe33bc40cd4db9f2099c5674de46e386f35100
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-12-10 23:35:04 -0500

    done 1398C

--- Codeforces/1398C.cpp
+++ Codeforces/1398C.cpp
@@ -1,16 +1,17 @@
 // https://codeforces.com/problemset/problem/1398/C
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXN = 1e5 + 1;
-int t, n; long long pre[MAXN]; string a; map<int, int> m;
+const int MAXN = 1e5;
+int t, n, pre[MAXN]; string a; map<int, int> m;
 
 int main() {
     cin >> t;
     while (t--) {
         cin >> n >> a; memset(pre, 0, sizeof(pre)); m.clear(); 
-        long long ans = 0; m[0] = 1;
-        for (int i = 1; i <= n; ++i) pre[i] = pre[i - 1] + a[i - 1] - '0';
-        for (int i = 1; i <= n; ++i) ans += m[pre[i] - i], m[pre[i] - i]++;
+        long long ans = 0; pre[0] = a[0] - '0'; m[0] = 1;
+        for (int i = 1; i < n; ++i) pre[i] = pre[i - 1] + a[i] - '0';
+        for (int i = 0; i < n; ++i) m[pre[i] - i - 1]++;
+        for (auto& v : m) ans +=  1LL * v.second * (v.second - 1) / 2;
         cout << ans << endl;
     }
     return 0;

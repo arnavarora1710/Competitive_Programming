8bc06f40ea533feb195e743c0ee4099bd6099cdb
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-12-28 20:36:35 +0530

    done Ferris_Wheel

--- CSES/Sorting and Searching/Ferris_Wheel.cpp
+++ CSES/Sorting and Searching/Ferris_Wheel.cpp
@@ -7,17 +7,16 @@ using namespace std;
 
 #define int long long
 #define MULTI_TEST (0)
-int n, x, a[200005], vis[200005];
+int n, x, a[200005];
 
 void solve() {
     cin >> n >> x; for (int i = 0; i < n; ++i) cin >> a[i];
     sort(a, a + n); int i = 0, j = n - 1, ans = 0;
     while (i < j) {
-        if (a[i] + a[j] <= x) ans++, vis[i] = vis[j] = 1, i++, j--;
-        else --j;
+        if (a[i] + a[j] <= x) ans++, i++, j--;
+        else ans++, --j;
     }
-    for (int i = 0; i < n; ++i) ans += !vis[i];
-    cout << ans << endl;
+    cout << ans + (i == j) << endl;
 }
 
 signed main() {

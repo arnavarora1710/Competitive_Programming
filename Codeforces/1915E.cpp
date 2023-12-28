8251d00ce0dc246f827754e64db500b3d5b6c1f6
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-12-29 04:00:09 +0530

    done 1915E

---
+++ Codeforces/1915E.cpp
@@ -0,0 +1,39 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MULTI_TEST (1)
+const int maxn = 200005;
+int n, a[maxn]; map<int, int> m;
+
+void solve() {
+    cin >> n; m.clear();
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        if (i&1) a[i] *= -1;
+    }
+    int s = 0; m[0] = 1;
+    for (int i = 0; i < n; ++i) {
+        s += a[i];
+        if (m.count(s)) {
+            cout << "YES\n"; return;
+        }
+        m[s]++;
+    }
+    cout << "NO\n";
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    if (MULTI_TEST) cin >> t;
+    while (t--) solve();
+
+    return 0;
+}

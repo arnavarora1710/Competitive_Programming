424e8b4bc9ba9c96490d33df45ca13a6b35acbe9
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-12-29 03:20:09 +0530

    done 1915A

---
+++ Codeforces/1915A.cpp
@@ -0,0 +1,26 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MULTI_TEST (1)
+int a, b, c;
+
+void solve() {
+    cin >> a >> b >> c;
+    cout << (a^b^c) << endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    if (MULTI_TEST) cin >> t;
+    while (t--) solve();
+
+    return 0;
+}

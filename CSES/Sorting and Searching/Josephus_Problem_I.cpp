e452663582edcd83890f5d433c2b429f995e06a7
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-12-30 20:56:28 +0530

    done Josephus_Problem_I

---
+++ CSES/Sorting and Searching/Josephus_Problem_I.cpp
@@ -0,0 +1,38 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MULTI_TEST (0)
+int n;
+
+void solve() {
+    cin >> n;
+    for (int i = 2; i <= n; i += 2) cout << i << " ";
+    set<int> s; for (int i = 1; i <= n; i += 2) s.insert(i);
+    set<int>::iterator it = s.begin();
+    if (!(n&1)) advance(it, 1);
+    while (s.size() != 1) {
+        cout << *it << " ";
+        auto it1 = it;
+        if (it == prev(s.end())) it = s.begin(), advance(it, 1);
+        else if (it == prev(prev(s.end()))) it = s.begin();
+        else advance(it, 2);
+        s.erase(it1);
+    }
+    cout << *s.rbegin() << endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    if (MULTI_TEST) cin >> t;
+    while (t--) solve();
+
+    return 0;
+}

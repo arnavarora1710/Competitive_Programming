e2f913abc322dafbf91bb9a16d41ee0968d3e359
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-01-15 13:13:14 -0500

    done Weird_Algorithm

--- CSES/Introductory Problems/Weird_Algorithm.cpp
+++ CSES/Introductory Problems/Weird_Algorithm.cpp
@@ -14,7 +14,7 @@ int main() {
         // if n is odd, multiply it by 3 and add 1
         // if n is even, divide by 2
         // bit hack notes:
-        // 1) (right shifting a number by 2 is equivalent to division by 2)
+        // 1) (right shifting a number by 1 is equivalent to division by 2)
         // 2) (to check if a number is odd, check if its first bit is set)
         if (n&1) n = 3 * n + 1;
         else n >>= 1;

04fed0067eb6180f15d03598971ef089d653ab3b
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-01-15 13:08:56 -0500

    done Weird_Algorithm

--- CSES/Introductory Problems/Weird_Algorithm.cpp
+++ CSES/Introductory Problems/Weird_Algorithm.cpp
@@ -2,10 +2,21 @@
 using namespace std;
 
 int main() {
-    long n; cin >> n; cout << n << " ";
+    // take in input
+    // n may turn into a large number so use longs
+    long n; cin >> n; 
+
+    // print out the initial value
+    cout << n << " ";
+
+    // simulate the process as described in the problem
     while (n != 1) {
+        // if n is odd, multiply it by 3 and add 1
+        // if n is even, divide by 2
         if (n&1) n = 3 * n + 1;
         else n >>= 1;
+
+        // print out the current value
         cout << n << " ";
     }
     return 0;

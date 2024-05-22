c57e663952539728c6540abe5c18c79bca5fe5da
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-05-22 14:34:12 -0400

    done PRIME1

---
+++ SPOJ/PRIME1 - Prime Generator.cpp
@@ -0,0 +1,71 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+typedef pair<int, int> pi;
+#define f first
+#define s second
+#define mp make_pair
+#define MULTI_TEST (1)
+const int maxn = 2e5 + 5, inf = 1e18;
+
+int power(int x, unsigned int y, int p)
+{
+    int res = 1;
+    x = x % p;
+    while (y > 0) {
+        if (y & 1)
+            res = (res*x) % p;
+        y = y>>1;
+        x = (x*x) % p;
+    }
+    return res;
+}
+ 
+bool miillerTest(int d, int n) {
+    int a = 2 + rand() % (n - 4);
+    int x = power(a, d, n);
+    if (x == 1  || x == n-1)
+       return true;
+    while (d != n-1) {
+        x = (x * x) % n;
+        d *= 2;
+        if (x == 1) return false;
+        if (x == n-1) return true;
+    }
+    return false;
+}
+
+bool isPrime(int n, int k) {
+    if (n <= 1 || n == 4) return false;
+    if (n <= 3) return true;
+    int d = n - 1;
+    while (d % 2 == 0)
+        d /= 2;
+    for (int i = 0; i < k; i++)
+         if (!miillerTest(d, n))
+              return false;
+    return true;
+}
+
+void solve() {
+    int n, m; cin >> n >> m;  
+    for (int i = n; i <= m; ++i)
+        if (isPrime(i, 100))
+            cout << i << '\n';
+    cout << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    if (MULTI_TEST) cin >> t;
+    while (t--) solve();
+
+    return 0;
+}

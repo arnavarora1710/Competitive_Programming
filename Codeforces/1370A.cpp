0caddccba2ed2851e56c7a6c276e7e8ac5b1e032
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-07-16 16:22:21 -0400

    done 1370A

---
+++ Codeforces/1370A.cpp
@@ -0,0 +1,32 @@
+// https://codeforces.com/contest/1370/problem/A
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+typedef pair<int, int> pi;
+#define f first
+#define s second
+#define mp make_pair
+#define MULTI_TEST (1)
+const int maxn = 2e5 + 5, inf = 1e18;
+int n, a[maxn];
+
+void solve() {
+    cin >> n;
+    n -= n&1;
+    cout << (n >> 1) << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    if (MULTI_TEST) cin >> t;
+    while (t--) solve();
+
+    return 0;
+}

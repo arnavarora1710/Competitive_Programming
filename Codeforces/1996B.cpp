65907ece702bad22b8460cf739e22de9ae2a0c88
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-08-06 16:03:34 -0400

    done 1996B

---
+++ Codeforces/1996B.cpp
@@ -0,0 +1,36 @@
+// https://codeforces.com/contest/1996/problem/B
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+typedef pair<int, int> pi;
+#define f first
+#define s second
+#define mp make_pair
+#define MULTI_TEST (1)
+const int maxn = 2e5 + 5, inf = 1e18;
+int n, k;
+string a[maxn];
+
+void solve() {
+    cin >> n >> k;
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    for (int i = 0; i < n; i += k) {
+        for (int j = 0; j < n; j += k) cout << a[i][j];
+        cout << '\n';
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    if (MULTI_TEST) cin >> t;
+    while (t--) solve();
+
+    return 0;
+}

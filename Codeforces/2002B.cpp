d37558653e652bc781c4d8535702b83b515aa9cf
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-08-11 23:31:03 -0400

    done 2002B

---
+++ Codeforces/2002B.cpp
@@ -0,0 +1,36 @@
+// https://codeforces.com/contest/2002/problem/B
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+typedef pair<int, int> pi;
+#define f first
+#define s second
+#define mp make_pair
+#define MULTI_TEST (1)
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    for (int i = 0; i < n; ++i) cin >> b[i];
+    if (a == b) cout << "Bob\n";
+    else {
+        reverse(a.begin(), a.end());
+        cout << (a == b ? "Bob\n" : "Alice\n");
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    if (MULTI_TEST) cin >> t;
+    while (t--) solve();
+
+    return 0;
+}

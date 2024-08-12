18afd1292d1eec1459153745977467038e9fe2ba
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-08-12 01:27:09 -0400

    done 1025

---
+++ TIMUS_OJ/1025.cpp
@@ -0,0 +1,35 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+typedef pair<int, int> pi;
+#define f first
+#define s second
+#define mp make_pair
+#define MULTI_TEST (0)
+const int maxn = 105, inf = 1e18;
+int n, a[maxn];
+
+void solve() {
+    cin >> n;
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    sort(a, a + n);
+    int ans = 0;
+    for (int i = 0; i < (n + 1) / 2; ++i) 
+        ans += (a[i] + 1) / 2;
+    cout << ans;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    if (MULTI_TEST) cin >> t;
+    while (t--) solve();
+
+    return 0;
+}

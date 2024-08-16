a943760bf0ae47ca9c996468867e0eee4dbe7371
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-08-16 01:33:17 -0400

    done 1946D

--- Codeforces/1946D.cpp
+++ Codeforces/1946D.cpp
@@ -9,41 +9,107 @@ using namespace std;
 typedef pair<int, int> pi;
 #define f first
 #define s second
+#define mp make_pair
 #define MULTI_TEST (1)
-int n, x, a[200005];
+const int maxn = 2e5 + 5, inf = 1e18;
+int n, k, a[maxn];
+
+int mergeIntervals(set<pi>& s) {
+    vector<pi> b(s.begin(), s.end());
+    vector<pi> c;
+    c.push_back(b[0]);
+    int j = 0;
+    for (int i = 1; i < b.size(); ++i) {
+        if (c[j].s >= b[i].f) 
+            c[j].s = max(c[j].s, b[i].s);
+        else {
+            j++;
+            c.push_back(b[i]);
+        }
+    }
+    set<pi> res(c.begin(), c.end());
+    s = res;
+    return res.size();
+}
 
 void solve() {
-    cin >> n >> x;
+    cin >> n >> k;
     for (int i = 0; i < n; ++i) cin >> a[i];
-    int o = 0, xo = 0;
-    for (int i = 0; i < n; ++i) o |= a[i], xo ^= a[i];
-    if (xo > x) cout << -1 << endl;
-    else if (o <= x) cout << n << endl;
+    int o = 0;
+    for (int i = 0; i < n; ++i) o = o | a[i];
+    if (o <= k) cout << n << '\n';
     else {
-        vector<vector<int>> bits(n, vector<int>(31));
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j <= 30; ++j) {
-                int bit = (a[i]) & (1 << j);
-                bits[i][j] = bit;
+        vector<set<pi>> inter(32);
+        vector<int> cnt(32);
+        for (int i = 0; i < 32; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cnt[i] += (a[j] >> i)&1;
+            }
+            for (int j = 0; j < n; ++j) {
+                if ((a[j] >> i)&1) {
+                    int x = j + 1;
+                    while (x < n && !((a[x] >> i)&1)) x++;
+                    inter[i].insert({j, x});
+                    j = x;
+                } else inter[i].insert({j, j});
             }
         }
-        int ans = 0;
-        for (int i = 30; i >= 0; --i) {
-            int cnt = 0;
-            for (int j = 0; j < n; ++j) cnt += bits[j][i];
-            if (x & (1 << i)) {
-                if (!(cnt&1)) {
-                    ans = max(ans, cnt/2);
+        set<pi> master;
+        int i = 31;
+        for (; i >= 0; --i) {
+            if ((k>>i)&1) break;
+            if (cnt[i]) {
+                if (cnt[i]&1) {
+                    cout << -1 << '\n';
+                    return;
+                } else {
+                    for (auto& v : inter[i]) 
+                        master.insert(v);
+                }
+            }
+        }
+        if (!master.empty()) mergeIntervals(master);
+        int ans = -1;
+        for (; i >= 0; --i) {
+            if (cnt[i]) {
+                if ((k>>i)&1) {
+                    if (cnt[i] % 2 == 0) {
+                        set<pi> tmp = master;
+                        for (auto& v : inter[i])
+                            tmp.insert(v);
+                        ans = max(ans, mergeIntervals(tmp));
+                    }
+                } else break;
+            } else {
+                if ((k >> i) & 1) {
+                    cout << mergeIntervals(master) << '\n';
+                    return;
+                }
+            }
+        }
+        bool ok = 1;
+        for (; i >= 0; --i) {
+            if ((k >> i)&1 && !cnt[i]) break;
+            if (!((k >> i)&1) && cnt[i]) {
+                if (cnt[i]&1) {
+                    ok = 0;
                     break;
                 }
-            } else {
-                if (!(cnt&1)) {
-                    ans = max(ans, cnt/2);
-                    if (cnt) break;
+                for (auto& v : inter[i])
+                    master.insert(v);
+            }
+            if ((k >> i)&1 && cnt[i]) {
+                if (cnt[i] % 2 == 0) {
+                    set<pi> tmp = master;
+                    for (auto& v : inter[i])
+                        tmp.insert(v);
+                    ans = max(ans, mergeIntervals(tmp));
                 }
             }
         }
-        cout << ans << endl;
+        if (ok) 
+            ans = max(ans, mergeIntervals(master));
+        cout << ans << '\n';
     }
 }
 

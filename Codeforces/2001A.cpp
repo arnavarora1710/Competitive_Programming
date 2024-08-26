5ad74dfe713dc0afcef03533867dfbef3e9dd780
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-08-26 10:25:35 -0400

    done 2001A

---
+++ Codeforces/2001A.cpp
@@ -0,0 +1,40 @@
+// https://codeforces.com/contest/2001/problem/A
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<class T> using oset = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+template<class T> using omset = tree<T, null_type, less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define int long long
+#define f first
+#define s second
+#define MULTI_TEST (1)
+
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+
+const int N = 200005, INF = 1e18, MOD = 1e9 + 7;
+int n, k, q, x, y, u, v, a[N];
+
+void solve() {
+    cin >> n;
+    map<int, int> cnt;
+    for (int i = 0; i < n; ++i) {
+        cin >> x;
+        cnt[x]++;
+    }
+    int sum = 0, mx = 0;
+    for (auto& x : cnt) sum += x.s, mx = max(mx, x.s);
+    cout << sum - mx << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int Q = 1; if (MULTI_TEST) cin >> Q;
+    while (Q--) solve();
+}
